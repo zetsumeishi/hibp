d87247e4388e8776275bd4c0d83157722f1c72b0
Cosmopanda <olivier.loustaunau@gmail.com>
2020-02-20 03:47:51 -0800

    Adding setup for pip, black and pre-commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name='hibpy',
+    packages=[],
+    version='0.1.0',
+    description='Python interface for HaveIBeenPwned',
+    author='Olivier Loustaunau',
+    license='MIT',
+    author_email='olivier.loustaunau@gmail.com',
+    url='https://github.com/Cosmopanda/hibp',
+    keywords=['hibp', 'package'],
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: Starting',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+)
