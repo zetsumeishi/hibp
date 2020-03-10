13806692bdd1073a3d00b5ecd1954dfc909f2515
zetsumeishi <olivier.loustaunau@gmail.com>
2020-03-10 12:51:01 -0700

    R9G, no more class

--- hibp/hibp.py
+++ hibp/hibp.py
@@ -1,5 +1,3 @@
-from time import sleep
-
 import requests
 
 from .constants import (
@@ -14,47 +12,43 @@ from .constants import (
 )
 
 
-class HIBP:
-    def __init__(self):
-        super(HIBP, self).__init__()
+def get(service, params=None, search_term=None):
+    query = f"{API_URL}/{service}"
+    if search_term:
+        query += f"/{search_term}"
+    if params:
+        query += f"?{params}"
 
-    def get(self, service, params=None, search_term=None):
-        query = f"{API_URL}/{service}"
-        if search_term:
-            query += f"/{search_term}"
-        if params:
-            query += f"?{params}"
+    response = requests.get(query, headers=HEADERS)
+    if response.status_code == 200:
+        return response
+    elif response.status_code == 404:
+        print("No results found")
+    else:
+        print(f"Something happened: {HTTP_CODES[str(response.status_code)]}")
 
-        response = requests.get(query, headers=HEADERS)
-        sleep(1)
-        if response.status_code == 200:
-            return response.json()
-        elif response.status_code == 404:
-            print("No results found")
-        else:
-            print(
-                f"Something happened: {HTTP_CODES[str(response.status_code)]}"
-            )
 
-    def breached_account(
-        self, email, truncate=True, domain=None, unverified=False
-    ):
-        params = f"truncateResponse={truncate}&includeUnverified={unverified}"
-        if domain:
-            params += f"&domain={domain}"
-        return self.get(BREACHED_ACCOUNT, params=params, search_term=email)
+def breached_account(email, truncate=True, domain=None, unverified=False):
+    params = f"truncateResponse={truncate}&includeUnverified={unverified}"
+    if domain:
+        params += f"&domain={domain}"
+    return get(BREACHED_ACCOUNT, params=params, search_term=email)
 
-    def breaches(self, domain=None):
-        params = ""
-        if domain:
-            params = f"domain={domain}"
-        return self.get(BREACHES, params=params)
 
-    def breach(self, breach_name):
-        return self.get(BREACH, search_term=breach_name)
+def breaches(domain=None):
+    params = ""
+    if domain:
+        params = f"domain={domain}"
+    return get(BREACHES, params=params)
 
-    def data_classes(self):
-        return self.get(DATA_CLASSES)
 
-    def paste_account(self, email):
-        return self.get(PASTE_ACCOUNT, search_term=email)
+def breach(breach_name):
+    return get(BREACH, search_term=breach_name)
+
+
+def data_classes():
+    return get(DATA_CLASSES)
+
+
+def paste_account(email):
+    return get(PASTE_ACCOUNT, search_term=email)
