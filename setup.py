57e7be89dcce0f41e0ac520a3574f2a415f2ccab
zetsumeishi <olivier.loustaunau@gmail.com>
2020-03-11 05:06:40 -0700

    Added setup.cfg and requests to package requirements

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/zetsumeishi/hibp",
     packages=setuptools.find_packages(exclude=("tests",)),
+    install_requires=["requests"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
