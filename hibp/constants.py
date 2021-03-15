6e53e3ec5468873f8d6b49c030267867c7e55759
zetsumeishi <olivier.loustaunau@gmail.com>
2021-03-16 00:47:36 +0100

    Change double to single quotes

--- hibp/constants.py
+++ hibp/constants.py
@@ -2,35 +2,35 @@ import os
 import sys
 
 try:
-    HIBP_API_KEY = os.environ.get("HIBP_API_KEY")
+    HIBP_API_KEY = os.environ.get('HIBP_API_KEY')
 except Exception:
     print(
-        "To use this package, please export your API key:\n"
-        "export HIBP_API_KEY='YOUR_API_KEY'"
+        'To use this package, please export your API key:\n'
+        'export HIBP_API_KEY=\'YOUR_API_KEY\''
     )
     sys.exit(0)
 
-HEADERS = {"User-Agent": "hibpy", "hibp-api-key": HIBP_API_KEY}
-VERSION = "v3"
-BASE_URL = "https://haveibeenpwned.com/"
-API_URL = f"https://haveibeenpwned.com/api/{VERSION}"
+HEADERS = {'User-Agent': 'hibpy', 'hibp-api-key': HIBP_API_KEY}
+VERSION = 'v3'
+BASE_URL = 'https://haveibeenpwned.com/'
+API_URL = f'https://haveibeenpwned.com/api/{VERSION}'
 
 # Services
 # All breaches for an account
-BREACHED_ACCOUNT = "breachedaccount"
+BREACHED_ACCOUNT = 'breachedaccount'
 # All breached sites on HIPB
-BREACHES = "breaches"
+BREACHES = 'breaches'
 # Single breached site
-BREACH = "breach"
+BREACH = 'breach'
 # All data classes (eg: Email addresses, passwords, etc...)
-DATA_CLASSES = "dataclasses"
+DATA_CLASSES = 'dataclasses'
 # All pastes for an account
-PASTE_ACCOUNT = "pasteaccount"
+PASTE_ACCOUNT = 'pasteaccount'
 
 HTTP_CODES = {
-    "400": "400 - Bad Request",
-    "401": "401 - Unauthorised",
-    "403": "403 - Forbidden",
-    "429": "429 - Too Many Requests",
-    "503": "503 - Service Unavailable",
+    '400': '400 - Bad Request',
+    '401': '401 - Unauthorised',
+    '403': '403 - Forbidden',
+    '429': '429 - Too Many Requests',
+    '503': '503 - Service Unavailable',
 }
--- tests/tests.py
+++ tests/tests.py
@@ -1,43 +1,41 @@
 import unittest
 
-from hibp.hibp import (
-    breached_account,
-    breaches,
-    breach,
-    data_classes,
-    paste_account,
-)
+from hibp.hibp import breach
+from hibp.hibp import breached_account
+from hibp.hibp import breaches
+from hibp.hibp import data_classes
+from hibp.hibp import paste_account
 
 BREACH_KEYS = [
-    ("Name", str),
-    ("Title", str),
-    ("Domain", str),
-    ("BreachDate", str),
-    ("AddedDate", str),
-    ("ModifiedDate", str),
-    ("PwnCount", int),
-    ("Description", str),
-    ("LogoPath", str),
-    ("DataClasses", list),
-    ("IsVerified", bool),
-    ("IsFabricated", bool),
-    ("IsSensitive", bool),
-    ("IsRetired", bool),
-    ("IsSpamList", bool),
+    ('Name', str),
+    ('Title', str),
+    ('Domain', str),
+    ('BreachDate', str),
+    ('AddedDate', str),
+    ('ModifiedDate', str),
+    ('PwnCount', int),
+    ('Description', str),
+    ('LogoPath', str),
+    ('DataClasses', list),
+    ('IsVerified', bool),
+    ('IsFabricated', bool),
+    ('IsSensitive', bool),
+    ('IsRetired', bool),
+    ('IsSpamList', bool),
 ]
 
 
 class HIBPTestCase(unittest.TestCase):
     def setUp(self):
-        self.content_type = "application/json; charset=utf-8"
+        self.content_type = 'application/json; charset=utf-8'
         self.status_code = 200
 
     def test_breached_account(self):
         response = breached_account(
-            "oliver@gmail.com", truncate=False, unverified=True
+            'oliver@gmail.com', truncate=False, unverified=True,
         )
         self.assertEqual(response.status_code, self.status_code)
-        self.assertEqual(response.headers["Content-Type"], self.content_type)
+        self.assertEqual(response.headers['Content-Type'], self.content_type)
         response = response.json()
         for elem in BREACH_KEYS:
             self.assertIsInstance(response[0][elem[0]], elem[1])
@@ -45,15 +43,15 @@ class HIBPTestCase(unittest.TestCase):
     def test_breaches(self):
         response = breaches()
         self.assertEqual(response.status_code, self.status_code)
-        self.assertEqual(response.headers["Content-Type"], self.content_type)
+        self.assertEqual(response.headers['Content-Type'], self.content_type)
         response = response.json()
         for elem in BREACH_KEYS:
             self.assertIsInstance(response[0][elem[0]], elem[1])
 
     def test_breach(self):
-        response = breach("Canva")
+        response = breach('Canva')
         self.assertEqual(response.status_code, self.status_code)
-        self.assertEqual(response.headers["Content-Type"], self.content_type)
+        self.assertEqual(response.headers['Content-Type'], self.content_type)
         response = response.json()
         for elem in BREACH_KEYS:
             self.assertIsInstance(response[elem[0]], elem[1])
@@ -61,22 +59,22 @@ class HIBPTestCase(unittest.TestCase):
     def test_data_classes(self):
         response = data_classes()
         self.assertEqual(response.status_code, self.status_code)
-        self.assertEqual(response.headers["Content-Type"], self.content_type)
+        self.assertEqual(response.headers['Content-Type'], self.content_type)
         response = response.json()
         for cls in response:
             self.assertIsInstance(cls, str)
 
     def test_paste_account(self):
-        response = paste_account("oliver@gmail.com")
+        response = paste_account('oliver@gmail.com')
         self.assertEqual(response.status_code, self.status_code)
-        self.assertEqual(response.headers["Content-Type"], self.content_type)
+        self.assertEqual(response.headers['Content-Type'], self.content_type)
         response = response.json()
-        self.assertIsInstance(response[0]["Id"], str)
-        self.assertIsInstance(response[0]["Source"], str)
-        self.assertIsInstance(response[0]["EmailCount"], int)
-        self.assertTrue(type(response[0]["Title"]) in [str, type(None)])
-        self.assertTrue(type(response[0]["Date"]) in [str, type(None)])
+        self.assertIsInstance(response[0]['Id'], str)
+        self.assertIsInstance(response[0]['Source'], str)
+        self.assertIsInstance(response[0]['EmailCount'], int)
+        self.assertTrue(type(response[0]['Title']) in [str, type(None)])
+        self.assertTrue(type(response[0]['Date']) in [str, type(None)])
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
